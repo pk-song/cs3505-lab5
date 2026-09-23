ff8fce7a719e0488d311360245d533afb63c5e53
Pankyo(PK) Song <spkcome@gmail.com>
2026-09-23 10:02:53 -0600

    Change the last sample value

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int main(){
     std::cout <<"Hello from Git!\n";
-    int i =1;
-    int y =2;
-    int j=3;
+    int i = 10;
+    int y = 2;
+    int j = 10;
 
     return 0;
 }
